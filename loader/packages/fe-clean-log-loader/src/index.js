13185aabad51b2e4f38f8151800b6f1d84e0e872
guozp <guo_zhipeng2@venusgroup.com.cn>
2022-12-09 21:48:35 +0800

    feat(fe-clean-log-loader): 增加参数说明

--- loader/packages/fe-clean-log-loader/src/index.js
+++ loader/packages/fe-clean-log-loader/src/index.js
@@ -1,22 +1,22 @@
 
 /* Loader就是一个函数， 当webpack解析资源时，会调用相应的Loader去处理， Loader接受到文件内容作为参数，返回内容出去。
 
-source 文件内容
-map： SourceMap
-meta： 别的Loader传递的数据 */
-const feCleanLogLoader = function (source) {
+content: 文件内容
+map: SourceMap
+meta: 别的Loader传递的数据 */
+const feCleanLogLoader = function (content, map, meta) {
 
   /*
-    第一个参数：err 代表是否有错误
-    第二个参数：source 处理后的内容
-    第三个参数：source-map 继续传递source-map
-    第四个参数：meta 给下一个loader传递参数
+    err: Error | null,           第一个参数: 代表是否有错误
+    content: string | Buffer,    第二个参数: 第二个参数是一个 string 或者 Buffer。
+    sourceMap?: SourceMap,       第三个参数: 第三个参数必须是一个可以被 this module 解析的 source map。
+    meta?: any                   第四个参数: 第四个参数，会被 webpack 忽略，可以是任何东西（例如一些元数据）。meta 给下一个loader传递参数
   */
-  this.callback(null, source, map, meta);
+  this.callback(null, content, map, meta);
   // 同步loader中不能进行异步操作
 
   // 将console.log替换为空
-  return source.replace(/console\.log\(.*\);?/g, "");
+  return content.replace(/console\.log\(.*\);?/g, "");
 }
 
 
